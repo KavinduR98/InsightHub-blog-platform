b980744b9b947906a208d5a88cd9cf84450c703e
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-03 07:10:46 +0530

    create Post repository

---
+++ blog-backend/src/main/java/com/ushan/blog_backend/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.ushan.blog_backend.repository;
+
+import com.ushan.blog_backend.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
