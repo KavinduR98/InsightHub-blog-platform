02b76e17464a374498db61f2f89f2ce4cfab39af
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-04 17:01:26 +0530

    implement User-Auth

---
+++ blog-backend/src/main/java/com/ushan/blog_backend/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.ushan.blog_backend.security;
+
+import com.ushan.blog_backend.domain.entities.User;
+import com.ushan.blog_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
