3425e7612a9715e4f8e869053f4f687111fad81e
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-04 15:58:58 +0530

    implement delete-Category endpoint

--- blog-backend/src/main/java/com/ushan/blog_backend/services/CategoryService.java
+++ blog-backend/src/main/java/com/ushan/blog_backend/services/CategoryService.java
@@ -3,8 +3,10 @@ package com.ushan.blog_backend.services;
 import com.ushan.blog_backend.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category> listCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
 }
