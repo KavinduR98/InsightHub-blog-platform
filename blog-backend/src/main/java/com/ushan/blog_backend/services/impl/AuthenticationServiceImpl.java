d7aaa55a66c0b68ad2209dc3c388b86363f0be98
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-06 12:51:52 +0530

    implement User login

--- blog-backend/src/main/java/com/ushan/blog_backend/services/impl/AuthenticationServiceImpl.java
+++ blog-backend/src/main/java/com/ushan/blog_backend/services/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ushan.blog_backend.services.impl;
 
 import com.ushan.blog_backend.services.AuthenticationService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -47,6 +48,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private String extractUsername(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
     }
 
     private Key getSigningKey(){
