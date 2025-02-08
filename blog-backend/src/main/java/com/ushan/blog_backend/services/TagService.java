931dbd6a79b27d4d106410793cbb90813d35db1c
@kavinduushan07 <kavinduushan07@gmail.com>
2025-02-08 22:59:08 +0530

    implement delete tag endpoint

--- blog-backend/src/main/java/com/ushan/blog_backend/services/TagService.java
+++ blog-backend/src/main/java/com/ushan/blog_backend/services/TagService.java
@@ -4,8 +4,10 @@ import com.ushan.blog_backend.domain.entities.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getTags();
     List<Tag> createTags(Set<String> tagNames);
+    Void deleteTag(UUID id);
 }
